db86c85f947b5ee938807d1c9ac3d1a0a7ae6f36
anggape <anggape.dev@gmail.com>
2022-05-18 12:44:51 +0700

    add prompt on first launch

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,5 +6,13 @@ if [ "$1" == "release" ]; then
     BUILD_TYPE=Release
 fi
 
+if grep -rnw 'CMakeLists.txt' -e '__raylib-template__'; then
+    printf '\n\n=================================================\n'
+    printf "Configuring project, this only happens once\n"
+    read -p 'Enter project name (no whitespace): ' PROJECT_NAME
+    ./scripts/setup.sh ${PROJECT_NAME}
+    printf '\n=================================================\n\n'
+fi
+
 cmake -B build -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
 cmake --build build
